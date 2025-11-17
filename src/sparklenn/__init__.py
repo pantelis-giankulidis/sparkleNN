22faf0301dfb4d400fcf26758e20e4c8415d16e4
pandelis-giankulidis <pantelis.giankoulidis+1@gmail.com>
2025-11-17 21:20:53 +0200

    Update README.md

--- src/sparklenn/__init__.py
+++ src/sparklenn/__init__.py
@@ -1,6 +1,6 @@
-from MLP import MLP
-from Layer import Layer
-from Neuron import Neuron
-from Value import Value
+from .MLP import MLP
+from .Layer import Layer
+from .Neuron import Neuron
+from .Value import Value
 
 __all__ = ['MLP', 'Layer', 'Neuron', 'Value']
