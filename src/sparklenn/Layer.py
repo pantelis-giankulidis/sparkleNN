7346895ff10eb0efdcc1d0b13d2672dc53eac8fd
pandelis-giankulidis <pantelis.giankoulidis+1@gmail.com>
2025-11-17 11:11:14 +0200

    Initial commit for sparkleNN library

---
+++ src/sparklenn/Layer.py
@@ -0,0 +1,8 @@
+from Neuron import Neuron
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
